4f530d1907c1e87757a34582c83376d7995c8fd8
Gofret <kanhasanali542@gmail.com>
2026-09-21 22:10:28 +0300

    jenga: staff-or-owner launch check via perms

--- commands/jenga.js
+++ commands/jenga.js
@@ -48,7 +48,7 @@ module.exports = {
         const sub = interaction.options.getSubcommand();
 
         if (sub === 'end') {
-            if (!interaction.member.permissions.has('ManageMessages') && interaction.user.id !== '1097807544849809408') {
+            if (!interaction.member.permissions.has('ManageMessages') && !require('../lib/perms').isOwner(interaction.user.id)) {
                 return interaction.reply({ content: '❌ No permission.', ephemeral: true });
             }
             activeGames.delete(interaction.guildId);
