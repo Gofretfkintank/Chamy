c62233b33d7bbec1136eea2ca75c6b3cba93718d
Gofret <kanhasanali542@gmail.com>
2026-09-21 22:10:15 +0300

    gartic: host-or-owner start check via perms

--- commands/gartic.js
+++ commands/gartic.js
@@ -94,7 +94,7 @@ module.exports = {
                 await i.update({ embeds: [lobbyEmbed()] });
             }
             if (i.customId === 'gar_start') {
-                if (i.user.id !== game.hostId && i.user.id !== '1097807544849809408') return i.reply({ content: '❌ Only host can start.', ephemeral: true });
+                if (!require('../lib/perms').canControlGame(i, game.hostId)) return i.reply({ content: '❌ Only host can start.', ephemeral: true });
                 if (game.players.size < 2) return i.reply({ content: '❌ At least 2 players needed!', ephemeral: true });
                 lobbyCollector.stop('start');
                 await i.deferUpdate();
