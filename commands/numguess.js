2790fe4ab19f4441fcef9cff35f208ce513503ef
Gofret <kanhasanali542@gmail.com>
2026-09-21 22:11:13 +0300

    numguess: host-or-owner check via perms

--- commands/numguess.js
+++ commands/numguess.js
@@ -76,7 +76,7 @@ module.exports = {
                 await i.update({ embeds: [lobbyEmbed()] });
             }
             if (i.customId === 'ng_start') {
-                if (i.user.id !== game.hostId && i.user.id !== '1097807544849809408') {
+                if (!require('../lib/perms').canControlGame(i, game.hostId)) {
                     return i.reply({ content: '❌ Only the host can start the game.', ephemeral: true });
                 }
                 if (game.players.size < 2) return i.reply({ content: '❌ At least 2 players required!', ephemeral: true });
